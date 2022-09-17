426f6c3e5bd52d44baf6e1f755bffcc37eb4777e
heiinhei911 <heiinhei911@gmail.com>
2022-09-16 23:26:40 -0400

    testing preparation

---
+++ client/src/App.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+
+test("renders home screen", () => {
+  render(<App />);
+
+  const nameElement = screen.getByRole("textbox", {
+    name: /enter your name:/i,
+  });
+
+  expect(nameElement).toBeInTheDocument();
+});
